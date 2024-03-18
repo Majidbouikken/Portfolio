0a794e7f2d0951117a65a6d4f77725abec0473f3
Majidbouikken <bouikkenmajid@gmail.com>
2024-03-18 22:33:52 +0200

    Update Works technologies

--- src/types/Technology.ts
+++ src/types/Technology.ts
@@ -2,8 +2,9 @@ enum Technology {
     "Kotlin", "Java", "Typescript", "Javascript", "Dart", "C#", "Python",
     "Android", "Flutter",
     "React", "Three.js", "Vue.js", "Tailwind", "Node.js",
-    "Spring", "Express.js", "PostgreSQL",
+    "Spring", "Express.js", "PostgreSQL", "MongoDB", "Firebase",
     "OpenCV", "Keras",
+    "AWS", "Azure",
     "Unity", "Blender",
     "Adobe XD",
 }
